7860228cbf79b927d035ad74eb54e4c8520ea115
Joe McGarry <joe.mcgarry@digital.cabinet-office.gov.uk>
2022-11-17 12:16:50 +0000

    creates inital screen for non-UK passport expiry info
    resolving conflicts

--- src/components/journeys/cic/nonUKPassportExpiry.ts
+++ src/components/journeys/cic/nonUKPassportExpiry.ts
@@ -3,10 +3,10 @@ import { AUTH_ROOT } from '../../../app.constants';
 import { EVENTS, PATH_NAMES } from './app.constants'
 import { getNext } from "./stateMachine/stateMachine";
 
-const TEMPLATE = "passportExpiry.njk"
+const TEMPLATE = "nonUkPassportExpiry.njk"
 const BACK = `${AUTH_ROOT}${PATH_NAMES.PHOTO_ID_SELECTION}`
 
-export async function ukPassportDateOfExpiry (req: Request, res: Response): Promise<void> {
+export async function nonUkPassportDateOfExpiry (req: Request, res: Response): Promise<void> {
   res.render(TEMPLATE, {
     back: BACK
   })
