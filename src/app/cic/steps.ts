ff4d165398dc5affac55b02a9e8790c510b1d3eb
Richa Misra <richa.misra@digital.cabinet-office.gov.uk>
2022-11-18 17:14:07 +0000

    Initial commit

--- src/app/cic/steps.ts
+++ src/app/cic/steps.ts
@@ -10,8 +10,7 @@ export const JOURNEYS= {
     next: "landingPage",
   },
   "/landingPage": {
-    resetJourney: true,
-    entryPoint: true,
+
     next: "photoIdSelection",
   },
   "/photoIdSelection": {
