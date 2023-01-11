aabbf144b67408aa191c43e306eba7b0042973eb
Joe McGarry <joe.mcgarry@digital.cabinet-office.gov.uk>
2023-01-11 15:59:22 +0000

    Updates photoIdSelection controller

--- src/app/cic/controllers/photoIdSelection.js
+++ src/app/cic/controllers/photoIdSelection.js
@@ -11,6 +11,7 @@ class PhotoIdSelectionController extends BaseController {
       req.sessionModel.set(APP.PHOTO_ID_OPTIONS.BRP, undefined);
       req.sessionModel.set(APP.PHOTO_ID_OPTIONS.UK_PHOTOCARD_DL, undefined);
       req.sessionModel.set(APP.PHOTO_ID_OPTIONS.OTHER_PASSPORT, undefined);
+      req.sessionModel.set(APP.PHOTO_ID_OPTIONS.CITIZEN_CARD, undefined);
 
       const action = req.form.values.photoIdChoice;
       req.sessionModel.set("photoIdChoice", action);
