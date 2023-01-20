2911c2d6e75485fabb1c14c0472d38f9f8720153
Joe McGarry <joe.mcgarry@digital.cabinet-office.gov.uk>
2023-01-20 14:21:31 +0000

    Added missing speech marks to no-console

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     root: true,
     extends: ["eslint:recommended", "prettier"],
     rules: {
-        "no-console": off,
+        "no-console": "off",
         "padding-line-between-statements": [
             "error",
             { blankLine: "any", prev: "*", next: "*" },
