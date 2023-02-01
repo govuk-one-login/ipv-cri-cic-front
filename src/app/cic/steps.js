3fc9c7f34f6f2a9e49d668eefe8511b91b65371e
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2023-02-01 08:58:22 +0000

    Updated steps with editable

--- src/app/cic/steps.js
+++ src/app/cic/steps.js
@@ -30,65 +30,88 @@ module.exports = {
   "/photoIdSelection": {
     fields: ['photoIdChoice'],
     controller: photoIdSelect,
+    editable: true,
+    editBackStep: "checkDetails",
     next: photoIdSelect.prototype.next
   },
   "/passportDetails": {
     fields: ["passportExpiryDate"],
     controller: passportDetails,
+    editable: true,
+    editBackStep: "checkDetails",
     next: passportDetails.prototype.next
   },
   "/nonUKPassportDetails": {
     fields: ["nonUKPassportExpiryDate"],
     controller: nonUKPassportDetails,
+    editable: true,
+    editBackStep: "checkDetails",
     next: passportDetails.prototype.next
   },
   "/photocardDlDetails": {
     fields: ["photocardDlExpiryDate"],
     controller: photocardDlDetails,
+    editable: true,
+    editBackStep: "checkDetails",
     next: photocardDlDetails.prototype.next
   },
   "/brpDetails": {
     fields: ["brpExpiryDate"],
     controller: brpDetails,
+    editable: true,
+    editBackStep: "checkDetails",
     next: brpDetails.prototype.next
   },
   "/euIdentityCardDetails": {
     fields: ["euIdCardExpiryDate"],
     controller: euIdentityCardDetails,
+    editable: true,
+    editBackStep: "checkDetails",
     next: euIdentityCardDetails.prototype.next
   },
   "/youngScotNecDetails": {
     fields: ["youngScotNationalEntitlementCardExpiryDate"],
     controller: youngScotNationalEntitlementCardDetails,
+    editable: true,
+    editBackStep: "checkDetails",
     next: youngScotNationalEntitlementCardDetails.prototype.next
   },
   "/citizenCardDetails": {
     fields: ["citizenCardExpiryDate"],
     controller: citizenCardDetails,
+    editable: true,
+    editBackStep: "checkDetails",
     next: citizenCardDetails.prototype.next
   },
   "/eeaPermanentResidencyCardDetails": {
     fields: ["eeaPrCardExpiryDate"],
     controller: eeaPermanentResidencyCard,
+    editable: true,
+    editBackStep: "checkDetails",
     next: eeaPermanentResidencyCard.prototype.next
   },
   "/euPhotocardDlDetails": {
     fields: ["euPhotocardDlExpiryDate"],
     controller: euPhotocardDlDetails,
+    editable: true,
+    editBackStep: "checkDetails",
     next: euPhotocardDlDetails.prototype.next
   },
   '/photoIdExpiry': {
-
     next: "/photoIdSelection"
   },
   "/nameEntry": {
     fields: ["surname", "firstName", "middleName"],
     controller: nameEntry,
+    editable: true,
+    editBackStep: "checkDetails",
     next: nameEntry.prototype.next
   },
   "/dateOfBirth": {
     fields: ["dateOfBirth"],
     controller: dobEntry,
+    editable: true,
+    editBackStep: "checkDetails",
     next: dobEntry.prototype.next
   },
   "/checkDetails": {
