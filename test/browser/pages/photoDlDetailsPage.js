160065c2c1d6abca9e678b73a6a898b7c4d94f80
shenry99 <sadie.henry@digital.cabinet-office.gov.uk>
2023-02-09 09:39:43 +0000

    F2F-257 Unhappy Path - UKPhotoDL Date Expiration

--- test/browser/pages/photoDlDetailsPage.js
+++ test/browser/pages/photoDlDetailsPage.js
@@ -9,28 +9,23 @@ module.exports = class PlaywrightDevPage {
 
 
   async isCurrentPage() {
-    console.log(">>In photoDlDetailsPage.js");
-    console.log(">>In isCurrentPage function");
-
     return await this.page.url() === this.url;
   }
 
   async continue() {
-    console.log(">>In photoDlDetailsPage.js");
-    console.log(">>In continue function");
     await this.page.click("#continue");
   }
 
   async expiryDateDay() {
-    await this.page.locator("#photocardDlExpiryDate-day").fill("31");
+    await this.page.locator("#photocardDlExpiryDate-day").fill("28");
   }
 
   async expiryDateMonth() {
-    await this.page.locator("#photocardDlExpiryDate-month").fill("12");
+    await this.page.locator("#photocardDlExpiryDate-month").fill("05");
   }
 
   async expiryDateYear() {
-    await this.page.locator("#photocardDlExpiryDate-year").fill("2025");
+    await this.page.locator("#photocardDlExpiryDate-year").fill("2033");
   }
 
 
