448c0a02db68de3c7947423a6f22af80a9dcacf6
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2023-02-13 14:33:31 +0000

    Added validations to steps.js

--- src/app/cic/steps.js
+++ src/app/cic/steps.js
@@ -11,9 +11,9 @@ const nameEntry = require("./controllers/nameEntry");
 const dobEntry = require("./controllers/dateOfBirth");
 const checkDetails = require('./controllers/checkDetails');
 const root = require("./controllers/root");
+const {APP} = require("../../lib/config");
 
 module.exports = {
-
   "/": {
     resetJourney: true,
     reset: true,
@@ -23,105 +23,258 @@ module.exports = {
     next: "landingPage",
   },
   "/landingPage": {
-
     next: "photoIdSelection",
   },
   "/photoIdSelection": {
-    fields: ['photoIdChoice'],
     controller: photoIdSelect,
     editable: true,
     editBackStep: "checkDetails",
+    fields: ["photoIdChoice"],
     invalidates: [
-      "brpExpiryDate",
-      "citizenCardExpiryDate",
-      "euIdCardExpiryDate",
-      "euPhotocardDlExpiryDate",
-      "nonUKPassportExpiryDate",
       "passportExpiryDate",
+      "nonUKPassportExpiryDate",
       "photocardDlExpiryDate",
-      "youngScotNationalEntitlementCardExpiryDate"
+      "brpExpiryDate",
+      "euIdCardExpiryDate",
+      "youngScotNationalEntitlementCardExpiryDate",
+      "citizenCardExpiryDate",
+      "euPhotocardDlExpiryDate",
     ],
-    next: photoIdSelect.prototype.next
+    next: [
+      {
+        field: "photoIdChoice",
+        value: APP.PHOTO_ID_OPTIONS.UK_PASSPORT,
+        next: APP.PATHS.PASSPORT_DETAILS,
+      },
+      {
+        field: "photoIdChoice",
+        value: APP.PHOTO_ID_OPTIONS.BRP,
+        next: APP.PATHS.BRP_DETAILS,
+      },
+      {
+        field: "photoIdChoice",
+        value: APP.PHOTO_ID_OPTIONS.UK_PHOTOCARD_DL,
+        next: APP.PATHS.PHOTOCARD_DL_DETAILS,
+      },
+      {
+        field: "photoIdChoice",
+        value: APP.PHOTO_ID_OPTIONS.OTHER_PASSPORT,
+        next: APP.PATHS.NON_UK_PASSPORT_DETAILS,
+      },
+      {
+        field: "photoIdChoice",
+        value: APP.PHOTO_ID_OPTIONS.EU_PHOTOCARD_DL,
+        next: APP.PATHS.EU_PHOTOCARD_DL_DETAILS,
+      },
+      {
+        field: "photoIdChoice",
+        value: APP.PHOTO_ID_OPTIONS.CITIZEN_CARD,
+        next: APP.PATHS.CITIZEN_CARD_DETAILS,
+      },
+      {
+        field: "photoIdChoice",
+        value: APP.PHOTO_ID_OPTIONS.YOUNG_SCOT_NATIONAL_ENTITLEMENT_CARD,
+        next: APP.PATHS.YOUNG_SCOT_NATIONAL_ENTITLEMENT_CARD_DETAILS,
+      },
+
+      {
+        field: "photoIdChoice",
+        value: APP.PHOTO_ID_OPTIONS.EU_IDENTITY_CARD,
+        next: APP.PATHS.EU_IDENTITY_CARD_DETAILS,
+      },
+    ],
   },
+
   "/passportDetails": {
     fields: ["passportExpiryDate"],
     controller: passportDetails,
     editable: true,
     editBackStep: "checkDetails",
-    next: passportDetails.prototype.next
+    next: [
+      {
+        field: "passportExpiryDate",
+        op: "before",
+        value: "18 months ago",
+        next: "photoIdExpiry",
+      },
+      {
+        field: "passportExpiryDate",
+        op: "after",
+        value: "10 years",
+        next: "photoIdExpiry",
+      },
+      "nameEntry",
+    ],
   },
   "/nonUKPassportDetails": {
     fields: ["nonUKPassportExpiryDate"],
     controller: nonUKPassportDetails,
     editable: true,
     editBackStep: "checkDetails",
-    next: passportDetails.prototype.next
+    next: [
+      {
+        field: "nonUKPassportExpiryDate",
+        op: "before",
+        value: "today",
+        next: "photoIdExpiry",
+      },
+      {
+        field: "nonUKPassportExpiryDate",
+        op: "after",
+        value: "10 years",
+        next: "photoIdExpiry",
+      },
+      "nameEntry",
+    ],
   },
   "/photocardDlDetails": {
     fields: ["photocardDlExpiryDate"],
     controller: photocardDlDetails,
     editable: true,
     editBackStep: "checkDetails",
-    next: photocardDlDetails.prototype.next
+    next: [
+      {
+        field: "photocardDlExpiryDate",
+        op: "before",
+        value: "today",
+        next: "photoIdExpiry",
+      },
+      {
+        field: "photocardDlExpiryDate",
+        op: "after",
+        value: "10 years",
+        next: "nameEntry",
+      },
+      "photoIdExpiry",
+    ],
   },
   "/brpDetails": {
     fields: ["brpExpiryDate"],
     controller: brpDetails,
     editable: true,
     editBackStep: "checkDetails",
-    next: brpDetails.prototype.next
+    next: [
+      {
+        field: "brpExpiryDate",
+        op: "before",
+        value: "today",
+        next: "photoIdExpiry",
+      },
+      {
+        field: "brpExpiryDate",
+        op: "after",
+        value: "2025-01-01",
+        next: "photoIdExpiry",
+      },
+      "nameEntry",
+    ],
   },
   "/euIdentityCardDetails": {
     fields: ["euIdCardExpiryDate"],
     controller: euIdentityCardDetails,
     editable: true,
     editBackStep: "checkDetails",
-    next: euIdentityCardDetails.prototype.next
+    next: [
+      {
+        field: "euIdCardExpiryDate",
+        op: "before",
+        value: "today",
+        next: "photoIdExpiry",
+      },
+      {
+        field: "euIdCardExpiryDate",
+        op: "after",
+        value: "75 years",
+        next: "photoIdExpiry",
+      },
+      "nameEntry",
+    ],
   },
   "/youngScotNecDetails": {
     fields: ["youngScotNationalEntitlementCardExpiryDate"],
     controller: youngScotNationalEntitlementCardDetails,
     editable: true,
     editBackStep: "checkDetails",
-    next: youngScotNationalEntitlementCardDetails.prototype.next
+    next: [
+      {
+        field: "youngScotNationalEntitlementCardExpiryDate",
+        op: "before",
+        value: "today",
+        next: "photoIdExpiry",
+      },
+      {
+        field: "youngScotNationalEntitlementCardExpiryDate",
+        op: "after",
+        value: "15 years",
+        next: "photoIdExpiry",
+      },
+      "nameEntry",
+    ],
   },
   "/citizenCardDetails": {
     fields: ["citizenCardExpiryDate"],
     controller: citizenCardDetails,
     editable: true,
     editBackStep: "checkDetails",
-    next: citizenCardDetails.prototype.next
+    next: [
+      {
+        field: "citizenCardExpiryDate",
+        op: "before",
+        value: "today",
+        next: "photoIdExpiry",
+      },
+      {
+        field: "citizenCardExpiryDate",
+        op: "after",
+        value: "4 years",
+        next: "photoIdExpiry",
+      },
+      "nameEntry",
+    ],
   },
   "/euPhotocardDlDetails": {
     fields: ["euPhotocardDlExpiryDate"],
     controller: euPhotocardDlDetails,
     editable: true,
     editBackStep: "checkDetails",
-    next: euPhotocardDlDetails.prototype.next
+    next: [
+      {
+        field: "nonUKPassportExpiryDate",
+        op: "before",
+        value: "today",
+        next: "photoIdExpiry",
+      },
+      {
+        field: "euPhotocardDlExpiryDate",
+        op: "after",
+        value: "75 years",
+        next: "photoIdExpiry",
+      },
+      "nameEntry",
+    ],
   },
-  '/photoIdExpiry': {
-    next: "/photoIdSelection"
+  "/photoIdExpiry": {
+    editable: true,
+    editBackStep: "checkDetails",
+     next: "/photoIdSelection",
   },
   "/nameEntry": {
+    editable: true,
+    editBackStep: "checkDetails",
     fields: ["surname", "firstName", "middleName"],
     controller: nameEntry,
-    editable: true,
-    editBackStep: "checkDetails",
-    next: nameEntry.prototype.next
+    next: "dateOfBirth",
   },
   "/dateOfBirth": {
-    fields: ["dateOfBirth"],
-    controller: dobEntry,
     editable: true,
     editBackStep: "checkDetails",
-    next: dobEntry.prototype.next
+    fields: ["dateOfBirth"],
+    controller: dobEntry,
+    next: "checkDetails",
   },
   "/checkDetails": {
     controller: checkDetails,
-    next: checkDetails.prototype.next
+    next: "done",
   },
-  '/done': {
-
-  },
-
+  "/done": {},
 };
