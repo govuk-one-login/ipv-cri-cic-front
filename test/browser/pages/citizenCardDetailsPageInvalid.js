1aaff3344243b19a5cd77912128ee6b8c410f7bb
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2023-02-17 12:20:44 +0000

    CitizenCard unhappy path dynamic dates

--- test/browser/pages/citizenCardDetailsPageInvalid.js
+++ test/browser/pages/citizenCardDetailsPageInvalid.js
@@ -17,16 +17,20 @@ module.exports = class PlaywrightDevPage {
     }
   
     async expiryDateDay() {
-      await this.page.locator("#citizenCardExpiryDate-day").fill("01");
+      const tomorrow = new Date().getDate() + 1
+      const expDay = tomorrow.toString()
+      await this.page.locator("#citizenCardExpiryDate-day").fill(expDay);
     }
   
     async expiryDateMonth() {
-      await this.page.locator("#citizenCardExpiryDate-month").fill("08");
+      const currentMonth = new Date().getMonth() + 1
+      const expMonth = currentMonth.toString()
+      await this.page.locator("#citizenCardExpiryDate-month").fill(expMonth);
     }
   
     async expiryDateYear() {
-      await this.page.locator("#citizenCardExpiryDate-year").fill("2029");
+      const futureYear = new Date().getFullYear() + 4
+      const expYear = futureYear.toString()
+      await this.page.locator("#citizenCardExpiryDate-year").fill(expYear);
     }
-  
-  
-  };
+};
