4f43d8e1134b6a7d727008e1b56feca70bc7c37a
richa-misra <richa.misra@digital.cabinet-office.gov.uk>
2023-02-20 13:04:27 +0000

    [F2F-122]: Aligning field names with openapi spec for claimedIdentity (#126)

--- src/app/cic/controllers/checkDetails.test.js
+++ src/app/cic/controllers/checkDetails.test.js
@@ -73,10 +73,10 @@ describe("CheckDetails controller", () => {
         const fullNameVal = req.sessionModel.get("middleName")? req.sessionModel.get("firstName") + " "+ req.sessionModel.get("middleName") + " "+ req.sessionModel.get("surname"):
           req.sessionModel.get("firstName") +  " "+ req.sessionModel.get("surname")
         const cicData ={
-          fullName: `${fullNameVal}`,
-          dateOfBirth: req.sessionModel.get("dateOfBirth"),
-          documentSelected:  req.sessionModel.get("photoIdChoice"),
-          dateOfExpiry: req.sessionModel.get("expiryDate")
+          full_name: `${fullNameVal}`,
+          date_of_birth: req.sessionModel.get("dateOfBirth"),
+          document_selected:  req.sessionModel.get("photoIdChoice"),
+          date_of_expiry: req.sessionModel.get("expiryDate")
         }
 
         await checkDetailsController.saveValues(req, res, next);
