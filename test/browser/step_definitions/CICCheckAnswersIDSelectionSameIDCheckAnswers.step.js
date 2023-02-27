4e8fe8bbe3f7648cc050b31a92fb066fc1f7efd6
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2023-02-27 16:46:21 +0000

    CMA -> ID selection same ID -> CMA

---
+++ test/browser/step_definitions/CICCheckAnswersIDSelectionSameIDCheckAnswers.step.js
@@ -0,0 +1,41 @@
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+const { expect } = require("chai");
+
+const { CheckDetailsPage, PhotoIdSelectionPageEdit, EuDrivingLicenceDetailsPageValidEdit }  = require("../pages");
+
+Given(/^the user has navigated to Check My Answers Page$/, async function () {
+    const cdPage = new CheckDetailsPage(await this.page);
+
+    expect(await cdPage.isCurrentPage()).to.be.true;
+
+});
+
+When(/^the change ID link is selected$/, async function () {
+    const cdPage = new CheckDetailsPage(await this.page);
+
+    await cdPage.changeIdType();
+
+})
+
+Then(/^the user is navigated back to the ID selection page$/, async function () {
+    const idSelection = new PhotoIdSelectionPageEdit(await this.page);
+
+    expect(await idSelection.isCurrentPage()).to.be.true;
+
+});
+
+
+Then(/^the user clicks continue with the same ID type selected$/, async function () {
+    const photoIdPage = new PhotoIdSelectionPageEdit(await this.page);
+  
+    await photoIdPage.continue();
+});
+
+
+Then(/^the user is navigated back to the Check My Answers Screen$/, async function () {
+    const cdPage = new CheckDetailsPage(await this.page);
+
+    expect(await cdPage.isCurrentPage()).to.be.true;
+
+});
