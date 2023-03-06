c200998300ae533e26e65bfc1e911ed12211aaf7
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2023-03-06 11:07:10 +0000

    Fixed further merge issues

--- test/browser/step_definitions/CICCitizenCardDateValidationPast.step.js
+++ test/browser/step_definitions/CICCitizenCardDateValidationPast.step.js
@@ -1,43 +1,39 @@
-const { Given, When, Then, And} = require("@cucumber/cucumber");
+const { Given, When, Then, And } = require("@cucumber/cucumber");
 
 const { expect } = require("chai");
 
-<<<<<<< HEAD:test/browser/step_definitions/CICCitizenCardDateValidationPast.step.js
-const {CitizenCardDetailsPageInvalidPast, PhotoIdExpiryPage} = require("../pages");
-=======
-const { CitizenCardDetailsPageInvalid } = require("../pages");
->>>>>>> main:test/browser/step_definitions/CICCitizenCardDateValidation.step.js
+const { CitizenCardDetailsPageInvalidPast } = require("../pages");
 
-  Given(/^the date entered is before the accepted CitizenCard expiration window$/, async function () {
+Given(/^the date entered is before the accepted CitizenCard expiration window$/, async function () {
 
-    const citizenCardDetails = new CitizenCardDetailsPageInvalidPast(await this.page);
-  
-    await citizenCardDetails.expiryDate();
+  const citizenCardDetails = new CitizenCardDetailsPageInvalidPast(await this.page);
 
-  });
+  await citizenCardDetails.expiryDate();
+
+});
 
 
-  When(/^the user clicks the continue button on the CitizenCard Past details Page$/, async function () {
+When(/^the user clicks the continue button on the CitizenCard Past details Page$/, async function () {
 
-    const citizenCardDetails = new CitizenCardDetailsPageInvalidPast(await this.page);
-  
-    expect(await citizenCardDetails.isCurrentPage()).to.be.true;
+  const citizenCardDetails = new CitizenCardDetailsPageInvalidPast(await this.page);
 
-    await citizenCardDetails.continue();
-  
-  });
-  
+  expect(await citizenCardDetails.isCurrentPage()).to.be.true;
 
-  Then(/^the user sees an inline error message displayed on the CitizenCard Page$/, async function () {
-        
-    const citizenCard = new CitizenCardDetailsPageInvalid(await this.page);
+  await citizenCardDetails.continue();
 
-    expect(await citizenCard.isCurrentPage()).to.be.true;
+});
 
-    const inlineError = 'There is a problem';
 
-    const error = await citizenCard.checkErrorText();
-      
-    expect(await error).to.equal(inlineError);
+Then(/^the user sees an inline error message displayed on the CitizenCard Page$/, async function () {
 
-  });
+  const citizenCard = new CitizenCardDetailsPageInvalidPast(await this.page);
+
+  expect(await citizenCard.isCurrentPage()).to.be.true;
+
+  const inlineError = 'There is a problem';
+
+  const error = await citizenCard.checkErrorText();
+
+  expect(await error).to.equal(inlineError);
+
+});
