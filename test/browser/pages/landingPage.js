7172a094abdbf65ecfa5b11fdfce394b8d30e48e
shenry99 <sadie.henry@digital.cabinet-office.gov.uk>
2023-03-07 16:10:55 +0000

    F2F-322 Call to PO web site from F2F screens

--- test/browser/pages/landingPage.js
+++ test/browser/pages/landingPage.js
@@ -1,7 +1,7 @@
 module.exports = class PlaywrightDevPage {
   /**
    * @param {import('@playwright/test').Page} page
-   */
+   */s
   constructor(page) {
     this.page = page;
     this.url = "http://localhost:5020/landingPage";
@@ -14,5 +14,17 @@ module.exports = class PlaywrightDevPage {
   async continue() {
     await this.page.click("#continue");
   }
+
+  get poLink() {
+  return this.page.locator('[href*="https://www.postoffice.co.uk/identity/in-branch-verification-service"]');
+
+  }
+
+   async postOfficeLink() {
+    await this.poLink.click();
+    await this.page.locator("#ensCloseBanner").click();
+    console.log(">> Clicked on Accept All button");
+  }
+
 
 };
---
+++ test/browser/step_definitions/CICLandingPagePO.step.js
@@ -0,0 +1,22 @@
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+const { expect } = require("chai");
+
+const { LandingPage} = require("../pages");
+
+
+Given(
+  /^the user wants to view their nearest post office that offers ID verification$/,
+   async function () {
+});
+
+When(/^the user clicks on the hyperlink$/, async function () {
+  const landingPage = new LandingPage(await this.page);
+
+   await landingPage.postOfficeLink();
+
+});
+
+
+ Then(/^they are redirected to the PO's own branch checking page$/, async function () {
+ });
