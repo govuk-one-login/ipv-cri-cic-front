3c6ad031fb42a165b4ebc7dce6bccb8d3d0aee1d
shenry99 <sadie.henry@digital.cabinet-office.gov.uk>
2023-03-10 10:27:13 +0000

    F2F-345 Setup Cucumber Automation Test Reporting

---
+++ test/utils/report.js
@@ -0,0 +1,18 @@
+var reporter = require('cucumber-html-reporter');
+
+var options = {
+        theme: 'bootstrap',
+        jsonFile: 'test/report/cucumber_report.json',
+        output: 'test/report/cucumber_report.html',
+        reportSuiteAsScenarios: true,
+        scenarioTimestamp: true,
+        launchReport: true,
+        metadata: {
+            "App Version":"0.3.2",
+            "Test Environment": "local",
+            "Platform": "Windows 10",
+            "Parallel": "Scenarios",
+            "Executed": "Remote"
+        }
+    };
+    reporter.generate(options);
