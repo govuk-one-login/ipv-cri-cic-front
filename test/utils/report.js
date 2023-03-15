ae38cbf3157a10ecc1a0b296ad235e0b9fb38aef
shenry99 <sadie.henry@digital.cabinet-office.gov.uk>
2023-03-15 14:50:35 +0000

    F2F-345 github pages and checks.yml update

--- test/utils/report.js
+++ test/utils/report.js
@@ -3,7 +3,7 @@ var reporter = require('cucumber-html-reporter');
 var options = {
         theme: 'bootstrap',
         jsonFile: 'test/report/cucumber_report.json',
-        output: 'test/report/cucumber_report.html',
+        output: 'test/report/index.html',
         reportSuiteAsScenarios: true,
         scenarioTimestamp: true,
         launchReport: true,
