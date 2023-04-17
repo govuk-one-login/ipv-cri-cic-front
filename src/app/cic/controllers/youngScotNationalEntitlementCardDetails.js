b7b48005ad98d2e4bf3b63e8d814ad6cd65c4037
Joe McGarry <joe.mcgarry@digital.cabinet-office.gov.uk>
2023-04-17 08:34:35 +0100

    Removes files and code referencing ID types

--- src/app/cic/controllers/youngScotNationalEntitlementCardDetails.js
+++
@@ -1,8 +0,0 @@
-const BaseController = require("hmpo-form-wizard").Controller;
-const DateControllerMixin = require("hmpo-components").mixins.Date;
-
-const DateController = DateControllerMixin(BaseController);
-
-class YoungScotNationalEntitlementCardDetailsController extends DateController {}
-
-module.exports = YoungScotNationalEntitlementCardDetailsController;
