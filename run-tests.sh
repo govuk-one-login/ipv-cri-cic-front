dd8581f1c400b13ddd7fa5e90927044d5b35e532
mudassar jafer <mjafer@deloitte.co.uk>
2023-07-21 14:58:10 +0100

    Updated the template, scripts and yarn.lock

--- run-tests.sh
+++ run-tests.sh
@@ -12,11 +12,7 @@ declare error_code
 # Github actions set to true for tests to run in headless mode
 export GITHUB_ACTIONS=true
 # shellcheck disable=SC2154
-#export IPV_STUB_URL=$(remove_quotes $CFN_CICIPVStubExecuteURL)start
-#https://ipvstub.review-c.build.account.gov.uk/start
-export IPV_STUB_URL="https://ipvstub.review-c.dev.account.gov.uk/start"
-
-echo "IPV Stub URL $IPV_STUB_URL"
+export IPV_STUB_URL=$(remove_quotes $CFN_CICIPVStubExecuteURL)start
 
 cd ./app; npm run test:browser:ci
 error_code=$?
