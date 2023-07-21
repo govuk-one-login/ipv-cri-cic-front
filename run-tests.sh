b532a9105dd8e734b299ec41f2cb58618358bca5
mudassar-jafer <133013208+mudassar-jafer@users.noreply.github.com>
2023-07-21 17:49:05 +0100

    [F2F -879 ] CIC front Test Container Script to run in the pipeline  (#247)
    * Adding tests to run in dev and build environments post merge
    
    * Adding tests to run in dev and build environments post merge
    
    * Test container initial commit
    
    * Updated the template, scripts and yarn.lock
    
    * Fixed package.json
    
    * Removing unnecessary exit from shell script
    
    * Remove yarn.lock file changes
    
    ---------
    
    Co-authored-by: mudassar jafer <mjafer@deloitte.co.uk>

---
+++ run-tests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# The CFN variables seem to include quotes when used in tests these quotes must 
+# be removed before assigning these variable.
+remove_quotes () {
+  echo "$1" | tr -d '"'
+}
+
+declare error_code
+# Github actions set to true for tests to run in headless mode
+export GITHUB_ACTIONS=true
+# shellcheck disable=SC2154
+export IPV_STUB_URL=$(remove_quotes $CFN_CICIPVStubExecuteURL)start
+
+cd ./app; npm run test:browser:ci
+
+cp -rf /app/test/reports $TEST_REPORT_ABSOLUTE_DIR
