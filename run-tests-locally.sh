7705533947975bc64aa2574df891fb57a1163fd0
mudassar-jafer <133013208+mudassar-jafer@users.noreply.github.com>
2023-07-24 14:38:36 +0100

    Shell script incorrectly reference app directory in cwd (#250)
    Co-authored-by: mudassar jafer <mjafer@deloitte.co.uk>

--- run-tests-locally.sh
+++ run-tests-locally.sh
@@ -30,7 +30,7 @@ then
     docker images $DockerImageName -q |xargs docker rmi
 
     docker build -f Dockerfile.test -t $DockerImageName .
-    docker run --rm --env-file docker_vars.env -v $(pwd)/results:/results $DockerImageName
+    docker run --rm --env-file docker_vars.env -v $(pwd)/test/reports:/results $DockerImageName
 else    
     echo "Please ensure you've got a stack name as the first argument after ./run_tests_locally.sh..."
     echo "E.g. ./run_tests_locally.sh cri-cic-api"
--- run-tests.sh
+++ run-tests.sh
@@ -8,12 +8,11 @@ remove_quotes () {
   echo "$1" | tr -d '"'
 }
 
-declare error_code
 # Github actions set to true for tests to run in headless mode
 export GITHUB_ACTIONS=true
 # shellcheck disable=SC2154
 export IPV_STUB_URL=$(remove_quotes $CFN_CICIPVStubExecuteURL)start
 
-cd ./app; yarn run test:browser:ci
+cd /app; yarn run test:browser:ci
 
 cp -rf /app/test/reports $TEST_REPORT_ABSOLUTE_DIR
