5de36c089b853b36f58f1b444ddfb2797baf422e
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2023-10-06 16:31:58 +0100

    Add unit tests and removed unused files

---
+++ src/assets/javascript/analytics/core.test.js
@@ -0,0 +1,22 @@
+const { expect } = require("chai");
+
+describe("Core", () => {
+  beforeEach(async () => {
+    global.window = {};
+    window.dataLayer = [];
+
+    require("./core");
+  });
+
+  afterEach(() => {
+    delete require.cache[
+      require.resolve("./core")
+    ];
+  });
+
+  it("Sends data to the data layer", function () {
+    global.window.DI.core.sendData({ foo: "bar" });
+    expect(window.dataLayer[0]).to.deep.equal({ foo: "bar" });
+  });
+
+});
