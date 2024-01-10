d836f2a0b99eaa3aaecdd12296f43b2cfc9dc837
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2024-01-10 15:47:55 +0000

    KIWI-1471: Adds extra copy for NO_PHOTO_ID

--- src/app/cic/controllers/nameEntry.js
+++ src/app/cic/controllers/nameEntry.js
@@ -4,7 +4,14 @@ class NameEntryController extends BaseController {
   locals(req, res, callback) {
       super.locals(req, res, (err, locals) => {
       const journeyType = req.sessionModel.get("journeyType");
+      locals.journeyType = journeyType;
       locals.title = req.translate(`nameEntry.${journeyType}.title`);
+
+      if (journeyType === "NO_PHOTO_ID") {
+        locals.introText = req.translate(`nameEntry.${journeyType}.introText`);
+        locals.insetText1 = req.translate(`nameEntry.${journeyType}.insetText1`);
+        locals.insetText2 = req.translate(`nameEntry.${journeyType}.insetText2`);
+      }
 
       callback(err, locals);
     });
