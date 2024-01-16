12734e80e709068b3e29c773de9692fc770f4a34
Jack Mardell <jack.mardell@digital.cabinet-office.gov.uk>
2024-01-16 14:14:25 +0000

    Added automated tests for verifying minimum character length for names.

---
+++ test/browser/features/BrowserBasedTests/UnHappyPath/CICNameEntryScreen.feature
@@ -0,0 +1,12 @@
+@mock-api:f2f-cic-success @success @browser
+Feature: The user enters an invalid name to be used as part of their claimed identity
+
+Background:
+    Given Authenticatable Anita is using the system
+    When they have provided their details
+    Then they should be redirected to the nameEntry
+
+Scenario: Error displayed for invalid name entry
+    Given there has been an invalid entry into the surname and first name fields
+    When the user clicks the NameEntry continue button
+    Then the user is shown an error message for invalid names
--- test/browser/step_definitions/CICNameEntry.step.js
+++ test/browser/step_definitions/CICNameEntry.step.js
@@ -1,8 +1,8 @@
-const { Given, When, Then} = require("@cucumber/cucumber");
+const { Given, When, Then } = require("@cucumber/cucumber");
 
 const { expect } = require("chai");
 
-const { NameEntryPage, DateOfBirthPage, EuDrivingLicenceDetailsPageValid }  = require("../pages");
+const { NameEntryPage, DateOfBirthPage, EuDrivingLicenceDetailsPageValid } = require("../pages");
 
 const userData = require("../support/cicUserData.json")
 
@@ -14,6 +14,19 @@ Given(
     await nameEntryPage.enterSurname(userData.lastName);
     await nameEntryPage.enterFirstName(userData.firstName);
     await nameEntryPage.enterMiddleName(userData.middleName);
+
+  }
+);
+
+Given(
+  /^there has been an invalid entry into the surname and first name fields$/,
+  async function () {
+    const nameEntryPage = new NameEntryPage(await this.page);
+
+    // Names must be more than 1 character long - making these names invalid
+    await nameEntryPage.enterSurname("A");
+    await nameEntryPage.enterFirstName("B");
+    await nameEntryPage.enterMiddleName("C");
 
   }
 );
@@ -38,34 +51,34 @@ Then(
 Given(/^only one mandatory name field has been entered$/, async function () {
   const nameEntryPage = new NameEntryPage(await this.page);
 
-  await nameEntryPage.enterSurname();
-  await nameEntryPage.enterMiddleName();
+  await nameEntryPage.enterSurname("Hartley");
+  await nameEntryPage.enterMiddleName("Robert");
 
 }
 );
 
 When(
-/^the user clicks the continue button in the NameEntry screen$/, async function () {
-const nameEntryPage = new NameEntryPage(await this.page);
+  /^the user clicks the continue button in the NameEntry screen$/, async function () {
+    const nameEntryPage = new NameEntryPage(await this.page);
 
-await nameEntryPage.continue();
+    await nameEntryPage.continue();
 
-});
+  });
 
 Then(
-/^the user sees an inline error message displayed in the NameEntry screen$/,
-async function () {
-  const nameEntryPage = new NameEntryPage(await this.page);
+  /^the user sees an inline error message displayed in the NameEntry screen$/,
+  async function () {
+    const nameEntryPage = new NameEntryPage(await this.page);
 
-  expect(await nameEntryPage.isCurrentPage()).to.be.true;
+    expect(await nameEntryPage.isCurrentPage()).to.be.true;
 
-  const inlineError = 'There is a problem';
+    const inlineError = 'There is a problem';
 
-  const error = await nameEntryPage.checkErrorText();
-    
-  expect(await error).to.equal(inlineError);
-  
-}
+    const error = await nameEntryPage.checkErrorText();
+
+    expect(await error).to.equal(inlineError);
+
+  }
 );
 
 Given(/^the user has navigated to the Name Entry screen$/, async function () {
@@ -77,16 +90,24 @@ Given(/^the user has navigated to the Name Entry screen$/, async function () {
 );
 
 When(/^the Back link is clicked on the Name Entry screen$/, async function () {
-const nameEntryPage = new NameEntryPage(await this.page);
+  const nameEntryPage = new NameEntryPage(await this.page);
 
-await nameEntryPage.back();
+  await nameEntryPage.back();
 
 });
 
-Then(/^the user is navigated back to the screen that they came from$/,async function () {
-const euDLDetailsPage = new EuDrivingLicenceDetailsPageValid(await this.page);
+Then(/^the user is navigated back to the screen that they came from$/, async function () {
+  const euDLDetailsPage = new EuDrivingLicenceDetailsPageValid(await this.page);
 
-expect(await euDLDetailsPage.isCurrentPage()).to.be.true;
+  expect(await euDLDetailsPage.isCurrentPage()).to.be.true;
+
+});
+
+Then(/^the user is shown an error message for invalid names$/, async function () {
+  const nameEntryPage = new NameEntryPage(await this.page);
   
-}
-);
+  expect(await nameEntryPage.checkErrorText()).to.contain("There is a problem");
+  expect(await nameEntryPage.getInvalidFirstNameErrorText()).to.contain("Your first name must be more than one character");
+  expect(await nameEntryPage.getInvalidMiddleNameErrorText()).to.contain("Your middle name must be more than one character");
+  expect(await nameEntryPage.getInvalidLastNameErrorText()).to.contain("Your last name must be more than one character");
+});
