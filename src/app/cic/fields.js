ad765c80f0820cd3e4d8951c30f09965817c9d61
Caitlin Cooling <40401118+Caitlin-cooling@users.noreply.github.com>
2024-01-22 14:31:14 +0000

    Revert "[KIWI-1208] - CIC & F2F | Investigate whether Form Wizard is validating dates correctly"

--- src/app/cic/fields.js
+++ src/app/cic/fields.js
@@ -33,7 +33,7 @@ module.exports = {
     journeyKey: "dateOfBirth",
     validate: [
       "required", "date",
-      { type: "before", arguments: [] },
+      { type: "before", arguments: [new Date().toISOString().split("T")[0]] },
       { type: "after", arguments: ["1904-02-12"] }
     ]
   }
