be731fb6fd7ac1ba2eaee473051a70b98e4423ac
Caitlin Cooling <40401118+Caitlin-cooling@users.noreply.github.com>
2024-01-23 17:04:16 +0000

    Kiwi 1471 routing (#511)
    * KIWI-1471: Valates name lenght > 1
    
    * KIWI-1471: Adds routing logic for PID vs no PID

--- src/app/cic/controllers/root.test.js
+++ src/app/cic/controllers/root.test.js
@@ -2,6 +2,9 @@ const BaseController = require("hmpo-form-wizard").Controller;
 const { expect } = require("chai");
 const { afterEach } = require("mocha");
 const RootController = require('./root.js');
+const { API } = require("../../../lib/config");
+
+console.log = sinon.fake();
 
 describe("RootController", () => {
   const rootController = new RootController({ route: '/test' });
@@ -91,6 +94,29 @@ describe("RootController", () => {
     expect(firstName).to.equal(undefined);
     expect(surname).to.equal(undefined);
     expect(dateOfBirth).to.equal(undefined);
+  });
+
+  it("should fetch the journey type from the session config endpoint", async () => {
+    req.axios.get = sinon.fake.resolves({ data: { journey_type: "FACE_TO_FACE" }});
+
+    await rootController.saveValues(req, res, next);
+
+    sinon.assert.calledWith(
+      req.axios.get,
+      `${API.PATHS.SESSION_CONFIG}`,
+      { headers: { "x-govuk-signin-session-id": req.session.tokenId } }
+    );
+    const journeyType = req.sessionModel.get("journeyType");
+    expect(journeyType).to.equal("FACE_TO_FACE");
+  });
+
+  it("should handle error if call to session config endpoint fails", async () => {
+    req.axios.get = sinon.fake.rejects("Error");
+
+    await rootController.saveValues(req, res, next);
+
+    sinon.assert.calledWith(console.log, "Error fetching journey type");
+    sinon.assert.called(next);
   });
 });
 
