6ba1ffdb57f943388a5561de23372b4a35a9cb11
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2024-01-23 16:51:10 +0000

    KIWI-1471: Adds routing logic for PID vs no PID

--- src/app/cic/controllers/root.js
+++ src/app/cic/controllers/root.js
@@ -1,11 +1,11 @@
 const { Controller: BaseController } = require("hmpo-form-wizard");
+const { API } = require("../../../lib/config");
 
 class RootController extends BaseController {
   async saveValues(req, res, next) {
     const sharedClaims = req.session?.shared_claims;
 
     if (sharedClaims) {
-
       if (sharedClaims?.name?.length > 0) {
         req.sessionModel.set("firstName", sharedClaims.name[0].nameParts[0].value);
         req.sessionModel.set("surname", sharedClaims.name[0].nameParts[2].value);
@@ -14,7 +14,19 @@ class RootController extends BaseController {
         req.sessionModel.set("dateOfBirth", sharedClaims.birthDate[0].value);
       }
     }
-    super.saveValues(req, res, next);
+
+    try {
+      const headers = {
+        "x-govuk-signin-session-id": req.session.tokenId
+      }
+
+      const { data } = await req.axios.get(`${API.PATHS.SESSION_CONFIG}`, { headers });
+      req.sessionModel.set("journeyType", data.journey_type);
+      return next()
+    } catch (error) {
+      console.log("Error fetching journey type", error)
+      return next(error)
+    }
   }
 }
 
