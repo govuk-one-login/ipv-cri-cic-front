f1c566bf17081e5e12944d6ffef8fec056fe463b
Caitlin Cooling <40401118+Caitlin-cooling@users.noreply.github.com>
2024-01-24 12:12:34 +0000

    Revert "Kiwi 1471 routing"

--- src/app/cic/controllers/nameEntry.js
+++ src/app/cic/controllers/nameEntry.js
@@ -1,19 +1,5 @@
 const BaseController = require("hmpo-form-wizard").Controller;
 
-class NameEntryController extends BaseController {
-  locals(req, res, callback) {
-      super.locals(req, res, (err, locals) => {
-      const journeyType = req.sessionModel.get("journeyType");
-
-      locals.journeyType = journeyType;
-      locals.title = req.translate(`nameEntry.${journeyType}.title`);
-      locals.introText = req.translate(`nameEntry.${journeyType}.introText`);
-      locals.insetText1 = req.translate(`nameEntry.${journeyType}.insetText1`);
-      locals.insetText2 = req.translate(`nameEntry.${journeyType}.insetText2`);
-
-      callback(err, locals);
-    });
-  }
-}
+class NameEntryController extends BaseController {}
 
 module.exports = NameEntryController; 
