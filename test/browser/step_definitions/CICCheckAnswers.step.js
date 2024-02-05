154b5dbe47d35760d6ec846eb6bcae2c31d29c22
jackmardell <147814888+jackmardell@users.noreply.github.com>
2024-02-05 15:16:54 +0000

    [KIWI-1528] Tests for NoPID journey landing page (#534)
    * Added tests for ac1 to ac4
    
    * fixed test for support Popup link
    
    * Lint and Prettier format changes
    
    * Revert "Lint and Prettier format changes"
    
    This reverts commit 096d67f8721cb0e9adde85126df19ea604d37771.
    
    * Reformatted a few files
    
    ---------
    
    Co-authored-by: MohamedHassan101 <mohamed.hassan@digital.cabinet-office.gov.uk>

--- test/browser/step_definitions/CICCheckAnswers.step.js
+++ test/browser/step_definitions/CICCheckAnswers.step.js
@@ -8,11 +8,10 @@ const ApiSupport = require("../support/ApiSupport");
 
 const TestHarness = require("../support/TestHarness");
 
+
 Given(/^the user has completed the previous CIC screens$/, async function () {
   const cpdPage = new CheckDetailsPage(await this.page);
-
   expect(await cpdPage.isCurrentPage()).to.be.true;
-
 });
 
 When(/^the user clicks the Check My Answers Submit button$/, { timeout: 2 * 50000 }, async function () {
@@ -21,8 +20,7 @@ When(/^the user clicks the Check My Answers Submit button$/, { timeout: 2 * 5000
   await cmPage.continue();
   this.state = await cmPage.setSessionState();
   this.authCode = await cmPage.setAuthCode();
-})
-
+});
 
 Given(/^I have retrieved the sessionTable data for my CIC session$/, { timeout: 2 * 50000 }, async function () {
   const testHarness = new TestHarness();
@@ -34,13 +32,13 @@ Given(/^I have retrieved the sessionTable data for my CIC session$/, { timeout: 
   this.authSessionState = session.authSessionState;
   this.authorizationCode = session.authorizationCode;
   this.redirectUri = session.redirectUri;
-})
+});
 
 
 Then(/^session details are correctly stored in DB$/, { timeout: 2 * 50000 }, async function () {
   expect(this.sessionId).to.not.be.null;
   expect(this.authSessionState).to.equal("CIC_AUTH_CODE_ISSUED");
-})
+});
 
 Then(/^the Verifiable Credential is correctly returned by the userInfo endpoint$/, { timeout: 2 * 50000 }, async function () {
   const apiSupport = new ApiSupport(process.env.CRI_F2F_API_URL);
@@ -48,5 +46,5 @@ Then(/^the Verifiable Credential is correctly returned by the userInfo endpoint$
   const userInfoRequest = await apiSupport.userInfoPostRequest(tokenRequest.data.access_token);
   const jwtToken  = await apiSupport.getJwtTokenUserInfo(JSON.stringify(userInfoRequest.data));
   await apiSupport.validateJwtToken(jwtToken);
-})
+});
 
