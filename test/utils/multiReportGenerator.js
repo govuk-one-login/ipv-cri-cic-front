f78bf1e68d6e5e9700b5ec18818d38d2c575de0c
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2024-02-29 13:08:53 +0000

    lint fix

--- test/utils/multiReportGenerator.js
+++ test/utils/multiReportGenerator.js
@@ -1,19 +1,5 @@
 const reporter = require("multiple-cucumber-html-reporter");
 var date = new Date();
-var currentDate =
-  date.getDate() +
-  "_" +
-  (date.getMonth() + 1) +
-  "_" +
-  date.getFullYear() +
-  "_" +
-  date.getHours() +
-  "_" +
-  date.getMinutes() +
-  "_" +
-  date.getSeconds() +
-  "_" +
-  date.getMilliseconds();
 
 var options = {
   jsonDir: "test/reports",
