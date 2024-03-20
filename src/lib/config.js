d9a953a7adf9e49a2a14a938b0ddab48738d41f4
di-fabs <fabien.saujot@digital.cabinet-office.gov.uk>
2024-03-20 09:32:26 +0000

    add new translations

--- src/lib/config.js
+++ src/lib/config.js
@@ -27,7 +27,7 @@ module.exports = {
       GA4_DISABLED: process.env.GA4_DISABLED || false,
       UA_DISABLED: process.env.UA_DISABLED || true,
     },
-    LANGUAGE_TOGGLE_ENABLED: process.env.LANGUAGE_TOGGLE_ENABLED || false,
+    LANGUAGE_TOGGLE_ENABLED: process.env.LANGUAGE_TOGGLE_ENABLED || "0",
   },
   PORT: process.env.PORT || 5020,
   SESSION_SECRET: process.env.SESSION_SECRET,
