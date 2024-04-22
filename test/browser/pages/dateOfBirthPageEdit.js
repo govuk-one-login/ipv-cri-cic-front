6c6d6d2416e028cbb004cacc234107c54106a083
Stelios-CB <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2024-04-22 16:45:42 +0100

    [KIWI-1643] - CIC | FE | UCD design Review (#619)
    * CIC UCD review update
    
    * Browser test fix
    
    * Replaced custom css id's with govuk spacing override classes
    
    * Add data-testid attribute to button elements and update related browser tests
    
    * Small test fix
    
    * Removed reference to disableFormSubmit() on input pages
    
    * Small fix
    
    * Removed width limit for error summary component

--- test/browser/pages/dateOfBirthPageEdit.js
+++ test/browser/pages/dateOfBirthPageEdit.js
@@ -13,7 +13,7 @@ module.exports = class PlaywrightDevPage {
   }
 
   async continue() {
-    await this.page.click("#continue");
+    await this.page.getByTestId("enter-dob-continue-btn").click();
   }
 
   async back() {
