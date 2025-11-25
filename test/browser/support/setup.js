4f036c59d4dd4f221c32d1530c1871bd11d22587
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2025-11-25 11:02:29 +0000

    Small pre-merge GHA update

--- test/browser/support/setup.js
+++ test/browser/support/setup.js
@@ -8,7 +8,7 @@ BeforeAll({ timeout: 2 * 5000 }, async function () {
     ? await chromium.launch()
     : await chromium.launch({
         // Not headless so we can watch test runs
-        headless: true,
+        headless: false,
         // Slow so we can see things happening
         //slowMo: 1000,
       });
