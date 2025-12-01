55685d33c7d911ca00ff6e620d91f709994f8561
DavidMcMichael-GDS <david.mcmichael1@digital.cabinet-office.gov.uk>
2025-12-01 12:22:05 +0000

    feat(DFC-892): Upgrade to v4 and set PII flags

--- src/app.js
+++ src/app.js
@@ -125,6 +125,10 @@ const { app, router } = setup({
       ],
     });
     app.use(setHeaders);
+    app.use((req, res, next) => {
+      res.locals.query = req.query; // Makes ?edit=true available as query.edit
+      next();
+    });
   },
   overloadProtection: overloadProtectionConfig,
   dev: true,
