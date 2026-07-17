49305bcfdaecff737fd715b72c4c8f99212c2568
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2026-07-17 13:44:42 +0100

    Removing obsolete flags

--- src/lib/config.js
+++ src/lib/config.js
@@ -23,10 +23,8 @@ module.exports = {
     },
     GTM: {
       GA4_ID: process.env.GOOGLE_ANALYTICS_4_GTM_CONTAINER_ID || "GTM-XXXXXXX",
-      UA_ID: "",
       ANALYTICS_COOKIE_DOMAIN: process.env.FRONTEND_DOMAIN || "localhost",
       GA4_ENABLED: true,
-      UA_ENABLED: false,
       ANALYTICS_DATA_SENSITIVE: false,
       GA4_PAGE_VIEW_ENABLED: process.env.GA4_PAGE_VIEW_ENABLED || true,
       GA4_FORM_RESPONSE_ENABLED: process.env.GA4_FORM_RESPONSE_ENABLED || true,
@@ -36,7 +34,6 @@ module.exports = {
       GA4_SELECT_CONTENT_ENABLED:
         process.env.GA4_SELECT_CONTENT_ENABLED || true,
     },
-    LANGUAGE_TOGGLE_DISABLED: false,
     DEVICE_INTELLIGENCE_ENABLED: true,
     DEVICE_INTELLIGENCE_DOMAIN: process.env.DEVICE_INTELLIGENCE_DOMAIN || "localhost",
   },
